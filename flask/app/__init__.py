8dc9f97994d1d540c246f6bf720f37661fdf2a40
Szymon Olewniczak <szyolewn@pg.edu.pl>
2021-09-27 13:00:24 +0200

    cleaning up

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,11 +3,11 @@ import os
 from flask import Flask, request
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY=os.getenv('MYSQL_HOST', default='dev'),
+        SECRET_KEY=os.getenv('SECRET_KEY', default='dev'),
         MYSQL_HOST=os.getenv('MYSQL_HOST'),
         MYSQL_USER=os.getenv('MYSQL_USER'),
         MYSQL_PASSWORD=os.getenv('MYSQL_PASSWORD'),
@@ -16,32 +16,8 @@ def create_app(test_config=None):
         MAX_NGRAMS=os.getenv('MAX_NGRAMS', default='5')
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
     # config types mapping
     app.config['MAX_NGRAMS'] = int(app.config['MAX_NGRAMS'])
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        # Use os.getenv("key") to get environment variables
-        app_name = os.getenv("APP_NAME")
-
-        if app_name:
-            return f"Hello from {app_name} running in a Docker container behind Nginx!"
-
-        return "Hello from Flask"
 
     from app import db
     db.init_app(app)
