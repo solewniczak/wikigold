be5a296e2fd352b27d8fa3ee9c4ee9e7a0373310
Szymon Olewniczak <szyolewn@pg.edu.pl>
2021-10-06 13:50:27 +0200

    add list test case

--- src/mwparallelparser/tests/runner.py
+++ src/mwparallelparser/tests/runner.py
@@ -6,12 +6,14 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))  # add tested 
 
 import test_doublebraces
 import test_header
+import test_list
 
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
 suite.addTests(loader.loadTestsFromModule(test_doublebraces))
 suite.addTests(loader.loadTestsFromModule(test_header))
+suite.addTests(loader.loadTestsFromModule(test_list))
 
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
