daf878675ae59542560911d3c10bf9eda33845b2
Szymon Olewniczak <szyolewn@pg.edu.pl>
2021-10-06 11:24:27 +0200

    additional test to header

--- src/mwparallelparser/tests/test_header.py
+++ src/mwparallelparser/tests/test_header.py
@@ -5,3 +5,7 @@ class HeaderTestCase(ParserTestCase):
     def test_basic(self):
         wikitext = '== Header ==\nContent'
         self.assertParsed(wikitext, ['Header', 'Content'])
+
+    def test_header_not_on_the_begining(self):
+        wikitext = 'Content1\n== Header ==\nContent2'
+        self.assertParsed(wikitext, ['Content1', 'Header', 'Content2'])
